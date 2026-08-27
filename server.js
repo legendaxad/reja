4c03a7966530f2c81079b43488a5fa4e59b95c1c
legendaxad <abdulaxadjon9@gmail.com>
2026-08-27 22:08:35 +0900

    feat:Added new files and fetures

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni Boshlash");
 const http = require("http");
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 // 1 Kirish Kodlari
 app.use(express.static("public"));
 app.use(express.json());
@@ -15,6 +25,9 @@ app.set("view engine", "ejs");
 app.post("/create-item", function (req, res) {
   console.log(req.body);
   res.json({ test: "success" });
+});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 app.get("/", function (req, res) {
   res.render("harid");
