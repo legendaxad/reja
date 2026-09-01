97f117a37ccc8a7c3974d8615e3df962519d7112
legendaxad <abdulaxadjon9@gmail.com>
2026-09-01 20:53:08 +0900

    feat:added .env privacy

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require("dotenv").config(); // Load environment variables at the top
 const http = require("http");
 const mongodb = require("mongodb");
+
 let db;
-const connectionString =
-  "mongodb+srv://kylemitcoder_db_user:bUh8OoU78FVOjUIV@cluster0.1pq5ryg.mongodb.net/Reja?appName=Cluster0";
+const connectionString = process.env.MONGO_URL;
+
 mongodb.connect(
   connectionString,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -13,10 +15,11 @@ mongodb.connect(
       db = client.db();
       console.log("MongoDBga muvaffaqiyatli ulandi");
       module.exports = client;
-      const app = require("./app");
 
+      const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = process.env.PORT || 3000;
+
       server.listen(PORT, function () {
         console.log(
           `This server is running successfully on port: ${PORT} , http://localhost:${PORT}`,
